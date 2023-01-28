a01947515e7cc562a5b866ef91b2b097dc1952fa
HowarthTom <tom.howarth9654@gmail.com>
2023-01-28 16:47:08 +0000

    restructures the scraper to process and save data correctly

--- main.py
+++ main.py
@@ -1,83 +1,134 @@
 import selenium
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
 import pandas as pd
+import os
+import json
 import time
-
-driver = webdriver.Chrome()
-time.sleep(2)
-show_list = []
-
-def get_show_score():
-    link = 'https://www.rottentomatoes.com/'
-    driver.get(link)
-
-    time.sleep(2)
-
-    try:
-        accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
-        accept_cookies_button.click()
-
-    except Exception as e:
-        print(e)
+from datetime import datetime 
 
 
-    driver.find_element(By.XPATH, '//*[@id="header-main"]/rt-header-nav/rt-header-nav-item[2]/a').click()
-    time.sleep(2)
-    shows = driver.find_elements(By.CLASS_NAME, 'js-tile-link')
-    for show in shows:
-        title = show.find_element(By.CLASS_NAME, 'p--small').get_attribute('innerText')
-        img = show.find_element(By.CLASS_NAME, 'posterImage').get_attribute('currentSrc')
-        critic_score = show.find_element(By.TAG_NAME, 'score-pairs').get_attribute('criticsscore')
-        audience_score = show.find_element(By.TAG_NAME, 'score-pairs').get_attribute('audiencescore')
 
-        link = show.get_attribute('href')
-        driver.execute_script("window.open('');")
-        driver.switch_to.window(driver.window_handles[1])
-        driver.get(link)
-        
+class Scraper:
+    def __init__(self, rt_link, tv_or_movie_selection, number_of_pages, additional_data):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.rt_link = rt_link
+        self.tv_or_movie_selection = tv_or_movie_selection
+        self.number_of_pages = number_of_pages
+        self.additional_data = additional_data
+        self.driver.get(self.rt_link)
         time.sleep(2)
+        self.accept_cookies()
 
+
+    def accept_cookies(self):
         try:
-            driver.find_element(By.CLASS_NAME, 'button--link js-clamp-toggle clamp-toggle__show-more').click()
-        
+            accept_cookies_button = self.driver.find_element(by=By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
+            accept_cookies_button.click()
+
         except Exception as e:
-            #print(e)
-            pass
+            print(e)
         
-        synopsis = driver.find_element(By.CSS_SELECTOR, '[class^="tv-series__series-info--synopsis clamp clamp-6 js-clamp clearfix"]').text
-        aired = driver.find_element(By.CLASS_NAME, 'h3.subtle').text[1:-1]
-        seasons = len(driver.find_elements(By.TAG_NAME, 'season-list-item'))
+        self.tv_or_movies()
 
-        global item
-        item = {
-            'Title': title,
+
+    def tv_or_movies(self):
+        tv_show_data = '//*[@id="header-main"]/rt-header-nav/rt-header-nav-item[2]/a'
+        movie_data = '//*[@id="header-main"]/rt-header-nav/rt-header-nav-item[1]/a'
+        if self.tv_or_movie_selection == 'tv_show_data':
+            tv_or_movies = tv_show_data
+        elif self.tv_or_movie_selection == 'movie_data':
+            tv_or_movies = movie_data
+        
+        self.driver.find_element(By.XPATH, tv_or_movies).click()
+        self.number_of_pages_to_scrape()
+
+
+    def number_of_pages_to_scrape(self):
+        for index in range(self.number_of_pages):
+            self.driver.find_element(By.XPATH, '//*[@id="main-page-content"]/div/div[5]/button').click()
+            time.sleep(2)    
+        
+        self.get_data()
+
+
+    def get_data(self):
+        title_cards = self.driver.find_elements(By.CLASS_NAME, 'js-tile-link')
+        for element in title_cards:
+            img = element.find_element(By.CLASS_NAME, 'posterImage').get_attribute('currentSrc')
+            title = element.find_element(By.CLASS_NAME, 'p--small').get_attribute('innerText')
+            audience_score = element.find_element(By.TAG_NAME, 'SCORE-PAIRS').get_attribute('audiencescore')
+            critic_score = element.find_element(By.TAG_NAME, 'SCORE-PAIRS').get_attribute('criticsscore')
+            href = element.get_attribute('href')
+
+            print(title, img)
+
+            dateTimeObj = datetime.now()
+            timestamp = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+
+            self.item = {
+                'Title': title,
+                'Audience Score': audience_score,
+                'Critic Score': critic_score,
+                'Timestamp': timestamp
+            }   
+            if self.additional_data == 'yes':
+                self.get_additional_data(href)
+            else:
+                continue
+            self.save_data(title, self.item, img)
+
+        
+    def get_additional_data(self, href):
+        self.driver.execute_script("window.open('');")
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.get(href)
+
+        if self.tv_or_movie_selection == 'tv_show_data':
+            try:
+                self.driver.find_element(By.CLASS_NAME, 'button--link js-clamp-toggle clamp-toggle__show-more').click()
+            except Exception as e:
+                #print(e)
+                pass
+            synopsis = self.driver.find_element(By.CSS_SELECTOR, '[class*="tv-series__series-info--synopsis clamp clamp-6 js-clamp"]').get_attribute('innerText')
+            premiere_date = self.driver.find_element(By.XPATH, '//*[@id="detail_panel"]/div/table/tbody/tr[2]/td[2]').get_attribute('innerText')
+        else:
+            synopsis = self.driver.find_element(By.XPATH, '//*[@id="movieSynopsis"]').get_attribute('innerText')
+            premiere_date = self.driver.find_element(By.TAG_NAME, 'TIME').get_attribute('innerText')
+        
+        dateTimeObj = datetime.now()
+        timestamp = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+
+        self.item.update({
             'Synopsis': synopsis,
-            'Img': img,
-            'Critic Score': critic_score,
-            'Audience Score': audience_score,
-            'Aired': aired,
-            'Seasons': seasons
-        }
-        show_list.append(item)
-        save_images(img, title)
+            'Premiere Date': premiere_date,
+            'Timestamp': timestamp
+        })
 
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+        self.driver.close()
+        self.driver.switch_to.window(self.driver.window_handles[0])
 
+
+    def save_data(self, title, item, img):
+        file_path = f'raw_data/{self.tv_or_movie_selection}/{title}'
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        with open(f'{file_path}/data.json', 'w') as fp:
+            json.dump(obj=item, indent=4, fp=fp)
+        img_data = requests.get(img).content
+        with open(f'{file_path}/{title}.jpg', 'wb') as handler:
+            handler.write(img_data)
         
-    
-    df = pd.DataFrame(show_list)
-    print(df)
-
-
-def save_images(img, title):
-    img_data = requests.get(img).content
-    with open(f'images/{title}.jpg', 'wb') as handler:
-        handler.write(img_data)
-    
+            
+        
 
 if __name__ == '__main__':
-    get_show_score()
+    rt_link = 'https://www.rottentomatoes.com/'
+    tv_or_movie_selection = 'movie_data'
+    number_of_pages = 2
+    additional_data = 'yes'
+   
+    scraper = Scraper(rt_link, tv_or_movie_selection, number_of_pages, additional_data)
+    scraper
